ca1152238a02d94570ea44218c84592fec0daec4
Ian Monroe <imonroe@kde.org>
2024-12-16 11:11:11 -0800

    possible publishing fix

--- upload.sh
+++ upload.sh
@@ -14,11 +14,12 @@ export SSH_REALLY_DELETE=1
 chmod 600 "$SSH_IDENTITY"
 
 go -C ./upload-vacuum/ build -o upload-vacuum .
-./upload-vacuum/upload-vacuum
+cd mkosi.output
+../upload-vacuum/upload-vacuum
 
 # For this script only
-export GNUPGHOME="$PWD/.secure_files/gpg"
-gpg --no-options --homedir="$GNUPGHOME" --import "$PWD/.secure_files/gpg.private.key"
+export GNUPGHOME="$PWD/../.secure_files/gpg"
+gpg --no-options --homedir="$GNUPGHOME" --import "$PWD/../.secure_files/gpg.private.key"
 REMOTE=$SSH_USER@$SSH_HOST:$SSH_PATH
 # You can use `ssh-keyscan origin.files.kde.org` to get the host key
 echo "origin.files.kde.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUjdH4S7otYIdLUkOZK+owIiByjNQPzGi7GQ5HOWjO6" >> ~/.ssh/known_hosts
@@ -27,12 +28,12 @@ echo "origin.files.kde.org ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILUjdH4S7otYIdLUk
 
 # More readable this way, ignore shellcheck
 # shellcheck disable=SC2129
-sha256sum -- mkosi.output/*.efi >> SHA256SUMS
-sha256sum -- mkosi.output/*.raw >> SHA256SUMS
-sha256sum -- mkosi.output/*.tar.zst >> SHA256SUMS
-sha256sum -- mkosi.output/*.torrent >> SHA256SUMS
+sha256sum -- *.efi >> SHA256SUMS
+sha256sum -- *.raw >> SHA256SUMS
+sha256sum -- *.tar.zst >> SHA256SUMS
+sha256sum -- *.torrent >> SHA256SUMS
 
 gpg --homedir="$GNUPGHOME" --output SHA256SUMS.gpg --detach-sign SHA256SUMS
 
-scp -i "$SSH_IDENTITY" mkosi.output/*.efi mkosi.output/*.raw mkosi.output/*.tar.zst mkosi.output/*.torrent "$REMOTE"
+scp -i "$SSH_IDENTITY" ./*.efi ./*.raw ./*.tar.zst ./*.torrent "$REMOTE"
 scp -i "$SSH_IDENTITY" SHA256SUMS SHA256SUMS.gpg "$REMOTE" # upload as last artifact to finalize the upload
