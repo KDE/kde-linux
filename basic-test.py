66e390f11e4c7518f14d82e5f4428c3078ad644e
Gabriel Barrantes <gabriel.barrantes.dev@outlook.com>
2025-10-05 08:17:53 -0600

    Update basic-test.py

--- basic-test.py
+++ basic-test.py
@@ -8,24 +8,36 @@ import sys
 import subprocess
 import os
 import time
+import threading
 
 from pathlib import Path
+
+test_completed = threading.Event()
+test_success = False
 
 class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/good':
-            sys.exit(0)
+            print("Test PASSED - received /good callback")
+            global test_success
+            test_success = True
+            test_completed.set()
         if self.path == '/bad':
-            print("==Received /bad callback==")
-            content_len = int(self.headers.get('Content-Length'))
-            body = self.rfile.read(content_len)
-            print(body.decode('utf-8'))
-            sys.exit(1)
+            print("Test FAILED - received /bad callback")
+            test_completed.set()
         self.send_response(200)
         self.end_headers()
 
 server = http.server.HTTPServer(server_address=('', 0), RequestHandlerClass=Handler)
-print("serving at port", server.server_port)
+print("Serving at port", server.server_port)
+
+# Start server in background thread so it's listening while we prepare the image
+def serve():
+    server.serve_forever()
+
+server_thread = threading.Thread(target=serve)
+server_thread.daemon = True
+server_thread.start()
 
 img = sys.argv[1]
 if not img:
@@ -38,13 +50,15 @@ if not efi_base:
     print("No EFI base image specified")
     sys.exit(1)
 
+print("Inject test into image")
 subprocess.check_call(['cp', '--reflink=auto', img, test_img])
-subprocess.check_call(['systemd-dissect', test_img, '--with', f'{os.path.dirname(os.path.realpath(__file__))}/basic-test-efi-addon.sh'],
-                      env={'PORT': str(server.server_port),
-                           'UKI': efi_base},
-                      stdout=sys.stdout, stderr=sys.stderr)
+#subprocess.check_call(['systemd-dissect', test_img, '--with', f'{os.path.dirname(os.path.realpath(__file__))}/basic-test-efi-addon.sh'],
+#                      env={'PORT': str(server.server_port),
+#                           'UKI': efi_base},
+#                      stdout=sys.stdout, stderr=sys.stderr)
 
 # I ought to point out that this leaks the process in case of failure. It will however get reaped by the docker container shutdown.
+print("Booting image in qemu")
 qemu = subprocess.Popen([
     "qemu-system-x86_64",
     "-drive",
@@ -56,13 +70,17 @@ qemu = subprocess.Popen([
     "host",
     "-bios",
     "/usr/share/OVMF/x64/OVMF.4m.fd",
+    "-append", f"kde-linux.basic-test=1 kde-linux.basic-test-callback=http://10.0.2.2:{server.server_port}/good",
 ])
 atexit.register(lambda: (qemu.kill()))
 
-server.timeout = 5 * 60 # 5 minutes
-server.handle_timeout = lambda: (qemu.kill(), sys.exit(1))
-while True: # kinda garbage but there seems to be no nice (non-private) poll-or-timeout api
-    server.handle_request()
-    time.sleep(8)
-qemu.kill()
-sys.exit(1) # if we get here we timed out = fail
+print("Waiting for call back...")
+if test_completed.wait(timeout=5*60):
+    server.shutdown()
+    qemu.kill()
+    sys.exit(0 if test_success else 1)
+else:
+    print("Test FAILED - Timeout")
+    server.shutdown()
+    qemu.kill()
+    sys.exit(1)
