0cd73ef26e7cb222c58b442c3d9a5fccf20c3090
Hadi Chokr <hadichokr@icloud.com>
2025-10-16 13:01:37 +0200

    Use yaml instead of hardcoding also tiny bugfix.

--- mkosi.extra/usr/bin/_kde-linux-command-not-found-handler.py
+++ mkosi.extra/usr/bin/_kde-linux-command-not-found-handler.py
@@ -1,63 +1,49 @@
 #!/usr/bin/python
 # SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
+# SPDX-FileCopyrightText: 2025 Hadi Chokr <hadichokr@icloud.com>
 
 import sys
+import yaml
+from pathlib import Path
 
-known_alternatives = {
-    "adduser" : "useradd",
-    "arp" : "ip neigh",
-    "cron" : "systemctl list-timers",
-    "dig" : "resolvectl query",
-    "egrep" : "rg",
-    "fgrep" : "rg -F",
-    "hostname" : "hostnamectl",
-    "host" : "resolvectl query",
-    "ifconfig" : "ip address",
-    "ifdown" : "ip link set [interface] down",
-    "ifup" : "ip link set [interface] up",
-    "iptunnel" : "ip tunnel",
-    "nameif" : "ip link",
-    "netstat" : "ss",
-    "nslookup" : "resolvectl query",
-    "route" : "ip route",
-    "service" :"systemctl",
-    "traceroute" : "tracepath",
-    "vi" : "vim"
-}
+CONFIG_PATH = Path("/usr/share/not-found/commands.yaml")
 
-unsupported_package_managers = [
-    "apt",
-    "apt-get",
-    "dnf",
-    "dpkg",
-    "nix",
-    "pacman",
-    "pamac",
-    "portage",
-    "rpm",
-    "yum",
-    "zypper"
-]
+def load_yaml(path):
+    try:
+        with open(path, "r") as f:
+            return yaml.safe_load(f)
+    except FileNotFoundError:
+        print(f"Configuration file not found: {path}")
+        sys.exit(1)
+    except yaml.YAMLError as e:
+        print(f"Error parsing YAML config: {e}")
+        sys.exit(1)
 
-available_package_managers = {
-    "brew" : "https://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Homebrew",
-    "nix" : "https://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Nix"
-}
+data = load_yaml(CONFIG_PATH)
+
+known_alternatives = data.get("known_alternatives", {})
+unsupported_package_managers = data.get("unsupported_package_managers", [])
+available_package_managers = data.get("available_package_managers", {})
+
+if len(sys.argv) < 2:
+    print("Usage: _kde-linux-command-not-found-handler.py <command>")
+    sys.exit(1)
 
 command = sys.argv[1]
 
 if command in known_alternatives:
-    print("\nKDE Linux does not include the “%s” tool.\n\nInstead, try using “%s”.\n" % (command, known_alternatives[command]))
-    exit(127)
+    print(f"\nKDE Linux does not include the “{command}” tool.\n\nInstead, try using “{known_alternatives[command]}”.\n")
+    sys.exit(127)
 
 if command in unsupported_package_managers:
-    print("\nKDE Linux does not include the “%s” package manager.\n\nGraphical software is available using the Discover app center. To learn how to install software that's not available in Discover, see\nhttps://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover.\n" % command)
-    exit(127)
+    print(f"\nKDE Linux does not include the “{command}” package manager.\n\nGraphical software is available using the Discover app center. To learn how to install software that's not available in Discover, see\nhttps://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover.\n")
+    sys.exit(127)
 
 if command in available_package_managers:
-    print("\nKDE Linux does not pre-install the “%s” package manager, but it can be added manually.\n\nTo do so, follow the instructions at %s\n" % (command, available_package_managers[command]))
-    exit(127)
+    print(f"\nKDE Linux does not pre-install the “{command}” package manager, but it can be added manually.\n\nTo do so, follow the instructions at {available_package_managers[command]}\n")
+    sys.exit(127)
 
-print ("\nKDE Linux does not include the “%s” command.\n\nIf you know it exists, and it's important for your workflow, learn about options for getting it at\nhttps://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Software_not_listed_above\n" % command)
-exit(127)
+print(f"\nKDE Linux does not include the “{command}” command.\n\nIf you know it exists, and it's important for your workflow, learn about options for getting it at\nhttps://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Software_not_listed_above\n")
+sys.exit(127)
+
