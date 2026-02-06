504c099665a5db09d1349811655a5c69aaa1946f
Nate Graham <nate@kde.org>
2026-02-06 15:51:11 -0700

    Replace old links to wiki with new ones pointing at docs page

--- mkosi.extra/usr/lib/command-not-found-handler.py
+++ mkosi.extra/usr/lib/command-not-found-handler.py
@@ -40,8 +40,8 @@ unsupported_package_managers = [
 ]
 
 available_package_managers = {
-    "brew" : "https://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Homebrew",
-    "nix" : "https://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Nix"
+    "brew" : "https://kde.org/linux/docs/more-software/#homebrew",
+    "nix" : "https://kde.org/linux/docs/more-software/#nix"
 }
 
 command = sys.argv[1]
@@ -51,12 +51,12 @@ if command in known_alternatives:
     exit(127)
 
 if command in unsupported_package_managers:
-    print("\nKDE Linux does not include the “%s” package manager.\n\nGraphical software is available using the Discover app center. To learn how to install software that's not available in Discover, see\nhttps://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover.\n" % command)
+    print("\nKDE Linux does not include the “%s” package manager.\n\nGraphical software is available using the Discover app center. To learn how to install software that's not available in Discover, see\nhttps://kde.org/linux/docs/more-software.\n" % command)
     exit(127)
 
 if command in available_package_managers:
     print("\nKDE Linux does not pre-install the “%s” package manager, but it can be added manually.\n\nTo do so, follow the instructions at %s\n" % (command, available_package_managers[command]))
     exit(127)
 
-print ("\nKDE Linux does not include the “%s” command.\n\nIf you know it exists, and it's important for your workflow, learn about options for getting it at\nhttps://community.kde.org/KDE_Linux/Install_software_not_available_in_Discover#Software_not_listed_above\n" % command)
+print ("\nKDE Linux does not include the “%s” command.\n\nIf you know it exists, and it's important for your workflow, learn about options for getting it at\nhttps://kde.org/linux/docs/more-software/#software-not-listed-above\n" % command)
 exit(127)
