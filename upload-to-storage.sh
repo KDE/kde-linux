cdaaad634e56c242b6c9b3ff3841247e9849787f
Harald Sitter <sitter@kde.org>
2026-02-16 02:56:29 +0100

    verbose--

--- upload-to-storage.sh
+++ upload-to-storage.sh
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 # SPDX-FileCopyrightText: 2025-2026 Harald Sitter <sitter@kde.org>
 
-set -eux
+set -eu
 
 if [ ! -d upload-tree ]; then
     mkdir upload-tree
