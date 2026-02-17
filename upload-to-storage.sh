6148c67e72a2d2567928ef6aaf85dfed47d415de
Harald Sitter <sitter@kde.org>
2026-02-17 06:18:33 +0100

    upload-to-storage: move directory instead of remove
    this should hopefully bypass permission issues caused by the fact that
    build runs as sudo but upload does not.

--- upload-to-storage.sh
+++ upload-to-storage.sh
@@ -5,7 +5,7 @@
 set -eu
 
 # Do not blow the lid off the storage for now. Reset the tree and only publish a select few files
-rm -rf upload-tree
+mv upload-tree upload-tree-old || true
 if [ ! -d upload-tree ]; then
     mkdir upload-tree
     for f in *.raw *.erofs *.efi; do
