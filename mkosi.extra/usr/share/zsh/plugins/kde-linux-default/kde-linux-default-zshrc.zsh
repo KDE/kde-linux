dfc79a4716071dd3e487a163fdae4196651a1af0
Daniele Md <dev.runway420@slmails.com>
2026-03-01 12:49:37 +0000

    Add various improvements to zsh config files
    This merge requests contains some changes to zsh configuration files:
    
    * `/etc/skel/.zshrc` now checks if `/usr/share/zsh/plugins/kde-linux-default/kde-linux-default-zshrc.zsh` exists before sourcing it. This is useful to avoid error messages when user create a container that shares the home directory
    * I have added key binding for **Page Up** and **Page down** to scroll through commands that start with user has currently typed. This is default behaviour in bash (at least in some distro, such as fedora), and I personally think it can be very useful
    * I have added a key binding for **Control+Delete** to delete the next word
    * I have slightly **modified zsh prompt** to make the current location path be colored in cyan, and when truncated, now it adds a `…/` prefix, to make it obvious the path was truncated
    * I have **disabled tab cycling** through completion suggestions. This would imitate the default behavior in bash, and I personally think it's more ergonomic. For example, who hasn't pressed tab multiple times because the completion is being slow, and in bash it's ok because the prompt hasn't changed, but in zsh or fish this means that you are getting a completion that might not have been the one you intended, and now you need to undo that.
    * I have **added a few aliases**, to make ls and grep have colored output by default, and to add ll, la, lla as shortcuts for useful ls commands (those are all aliases that debian based distros such as ubuntu have in their bashrc files)

--- mkosi.extra/usr/share/zsh/plugins/kde-linux-default/kde-linux-default-zshrc.zsh
+++ mkosi.extra/usr/share/zsh/plugins/kde-linux-default/kde-linux-default-zshrc.zsh
@@ -21,7 +21,15 @@ bindkey '\e[2~' overwrite-mode
 bindkey '^[[1;5D' backward-word     # Ctrl+Left
 bindkey '^[[1;5C' forward-word      # Ctrl+Right
 bindkey '^H' backward-kill-word     # Ctrl+Backspace
+bindkey '^[[3;5~' kill-word         # Ctrl+Delete
 WORDCHARS=${WORDCHARS//\/[&.;]} # Don't consider certain characters part of the word
+# Scroll through commands in history that start with current command line:
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey '\e[5~' up-line-or-beginning-search     # Page up
+bindkey '\e[6~' down-line-or-beginning-search   # Page down
 
 # Don't fail on non-matching globs - they may be used by a command internally.
 # This behaviour is more consistent with bash.
@@ -60,7 +68,7 @@ parse_git_info() {
   fi
 }
 
-local path_segment='[%f%F{yellow}%n@%m%f:%4~%f]%f'
+local path_segment='[%f%F{yellow}%n@%m%f:%F{cyan}%(4~|…/%3~|%~)%f]%f'
 local git_segment='$(parse_git_info)'
 local prompt_symbol='%(?. . %F{red}[%?]%f )%(#.#.$) '
 
@@ -69,3 +77,15 @@ PROMPT="${path_segment}${git_segment}${prompt_symbol}"
 # Turn on completions.
 autoload -U compinit
 compinit
+
+# Disable tab cycling through completions.
+zstyle ':completion:*' menu no
+
+# Add colored output for various commands.
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
+# Add various useful aliases.
+alias la='ls -A'
+alias ll='ls -l'
+alias lla='ls -lA'
