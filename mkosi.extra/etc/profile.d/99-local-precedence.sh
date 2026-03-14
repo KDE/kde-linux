105cd5ecbce621009cd131a179373570c4d8699e
Hadi Chokr <hadichokr@icloud.com>
2026-03-14 14:58:05 +0100

    Put ~/.local/bin in PATH
    Splits it out of the /opt/local MR to get this merged more easily.
    
    Closes https://invent.kde.org/kde-linux/kde-linux/-/issues/381

---
+++ mkosi.extra/etc/profile.d/99-local-precedence.sh
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+# SPDX-FileCopyrightText: 2026 Hadi Chokr <hadichokr@icloud.com>
+
+# Setup PATH to let ~/.local mask everything
+export PATH="$HOME/.local/bin:$HOME/.local/sbin:$PATH"
