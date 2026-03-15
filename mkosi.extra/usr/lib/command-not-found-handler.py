1d7b2ee03fd4844bf9b5735c6981e50eb13d89e7
Nate Graham <nate@kde.org>
2026-03-15 08:57:45 -0600

    Make command-not-found handler translatable
    Resolves #390

--- mkosi.extra/usr/lib/command-not-found-handler.py
+++ mkosi.extra/usr/lib/command-not-found-handler.py
@@ -3,6 +3,14 @@
 # SPDX-FileCopyrightText: 2025 Nate Graham <nate@kde.org>
 
 import sys
+import gettext
+
+gettext.install("kde-linux")
+
+moreSoftwareUrl = "https://kde.org/linux/docs/more-software"
+moreSoftwareHomebrewUrl = moreSoftwareUrl + "/#homebrew"
+moreSoftwareNixUrl = moreSoftwareUrl + "/#nix"
+moreSoftwareOtherUrl = moreSoftwareUrl + "/#software-not-listed-above"
 
 known_alternatives = {
     "adduser" : "useradd",
@@ -41,8 +49,8 @@ unsupported_package_managers = [
 ]
 
 available_package_managers = {
-    "brew" : "https://kde.org/linux/docs/more-software/#homebrew",
-    "nix" : "https://kde.org/linux/docs/more-software/#nix"
+    "brew" : moreSoftwareHomebrewUrl,
+    "nix" : moreSoftwareNixUrl
 }
 
 related_commands = {
@@ -61,16 +69,13 @@ if command in related_commands:
     command = related_commands[command]
 
 if command in known_alternatives:
-    print("\nKDE Linux does not include the “%s” tool.\n\nInstead, try using “%s”.\n" % (command, known_alternatives[command]))
-    exit(127)
+    message = gettext.gettext("KDE Linux does not include the “{}” tool.\n\nInstead, try using “{}”.".format(command, known_alternatives[command]))
+elif command in unsupported_package_managers:
+    message = gettext.gettext("KDE Linux does not include the “{}” package manager.\n\nGraphical software is available using the Discover app center. To learn how to install software that’s not available in Discover, see {}".format(command, moreSoftwareUrl))
+elif command in available_package_managers:
+    message = gettext.gettext("KDE Linux does not pre-install the “{}” package manager, but it can be added manually.\n\nTo do so, follow the instructions at {}".format(command, available_package_managers[command]))
+else:
+    message = gettext.gettext("KDE Linux does not include the “{}” command.\n\nIf you know it exists, and it’s important for your workflow, learn about options for getting it at {}".format(command, moreSoftwareOtherUrl))
 
-if command in unsupported_package_managers:
-    print("\nKDE Linux does not include the “%s” package manager.\n\nGraphical software is available using the Discover app center. To learn how to install software that's not available in Discover, see\nhttps://kde.org/linux/docs/more-software.\n" % command)
-    exit(127)
-
-if command in available_package_managers:
-    print("\nKDE Linux does not pre-install the “%s” package manager, but it can be added manually.\n\nTo do so, follow the instructions at %s\n" % (command, available_package_managers[command]))
-    exit(127)
-
-print ("\nKDE Linux does not include the “%s” command.\n\nIf you know it exists, and it's important for your workflow, learn about options for getting it at\nhttps://kde.org/linux/docs/more-software/#software-not-listed-above\n" % command)
+print("\n" + message + "\n")
 exit(127)
