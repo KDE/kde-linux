44c4244e0db804f3b46d1f4dff32af22b4b39f9e
Hadi Chokr <hadichokr@icloud.com>
2026-04-01 10:10:01 +0200

    Ups.

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
-$XGETTEXT --language=Python mkosi.extra/usr/lib/command-not-found-handler.py -o $podir/kde-linux.pot
+$XGETTEXT --language=Python mkosi.extra/usr/lib/command-not-found-handler.py --output=$podir/kde-linux.pot
 $XGETTEXT --language=Python mkosi.extra/usr/lib/kjar-install --join-existing --output=$podir/kde-linux.pot
