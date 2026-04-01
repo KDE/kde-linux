2c8655488333effc967ddcb11c2db9b52d2196ba
Hadi Chokr <hadichokr@icloud.com>
2026-04-01 09:02:21 +0200

    Use long options for gettext

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
-$XGETTEXT --language=Python mkosi.extra/usr/lib/command-not-found-handler.py -o $podir/kde-linux.pot
+$XGETTEXT --language=Python mkosi.extra/usr/lib/command-not-found-handler.py --output=$podir/kde-linux.pot
