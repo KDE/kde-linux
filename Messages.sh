a779a4bc18fd2ab118df21876d4590f82e283434
Hadi Chokr <hadichokr@icloud.com>
2026-04-01 10:08:39 +0200

    Make kjar-install a proper program

--- Messages.sh
+++ Messages.sh
@@ -2,4 +2,4 @@
 # SPDX-FileCopyrightText: None
 # SPDX-License-Identifier: CC0-1.0
 $XGETTEXT --language=Python mkosi.extra/usr/lib/command-not-found-handler.py -o $podir/kde-linux.pot
-$XGETTEXT --language=Shell mkosi.extra/usr/lib/kjar-install --join-existing --output=$podir/kde-linux.pot
+$XGETTEXT --language=Python mkosi.extra/usr/lib/kjar-install --join-existing --output=$podir/kde-linux.pot
